d29dd6ff5c190c8fc76221ff11dca3ecc8fc5a4f
Juan Ignacio Sotomayor <jignaciosotomayor205@gmail.com>
2024-11-07 18:02:05 -0300

    Se agrega un post para vehiculos con sus primeras verificaciones

--- back/vehiculos.js
+++ back/vehiculos.js
@@ -18,7 +18,23 @@ vehiculosRouter.get('/', async (req,res)=>{
         sql += ` WHERE ${filtros.join(" AND ")}`;
     }
 
-    console.log(sql)
     const [vehiculos] = await db.execute(sql,parametros)
     res.send({vehiculos})
+})
+
+vehiculosRouter.post("/", async (req,res)=>{
+    const { matricula, id_usuario, id_tipo_vehiculo } = req.body
+
+    if ( !matricula || !id_usuario || !id_tipo_vehiculo){
+        return res.status(400).send("Todos los campos deben de llenarse")
+    } 
+
+    const sql = await db.execute(
+        "insert into vehiculos (matricula,id_usuario,id_tipo_vehiculo) values(?,?,?)",
+        [matricula,id_usuario,id_tipo_vehiculo]
+    )
+
+    res
+    .status(201)
+    .send({vehiculo:{id_vehiculo:sql.insertId,matricula,id_usuario,id_tipo_vehiculo}})
 })
