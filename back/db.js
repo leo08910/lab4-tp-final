09da3d655a4e47f8a5e4daee3cc1ac093e40d730
Alejandro <gmz248alejandro@gmail.com>
2024-11-13 18:47:23 -0300

    Archivos organizados en carpetas

--- back/db.js
+++ back/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
 export let db;
 
 export async function conectarDB() {
-  db = await mysql.createConnection({ // en vez de crear una conxion unica cree un pool de conexiones donde con los metodos me ayuda a manejarlas en caso de errores
+  db = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
