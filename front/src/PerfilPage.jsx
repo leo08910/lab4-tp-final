9039d292bf45193081f467f6ca338e69705095db
AkumaxDDD.gaspar.ahumada101@gmail.com <AkumaxDDD>
2024-11-18 17:41:56 -0300

    agregue estilos tarifas

--- front/src/PerfilPage.jsx
+++ front/src/PerfilPage.jsx
@@ -1,15 +1,26 @@
-import { AuthRol } from "./Auth";
+import { AuthRol, useAuth } from "./Auth";
+import { useNavigate } from "react-router-dom";
 
 export const PerfilPage = () => {
+  const { sesion, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout(() => navigate("/")); // Al cerrar sesión, redirige a la página principal
+  };
   return (
     <>
-      <p>Mi perfil</p>
-      <AuthRol rol="admin">
+    <div>
+      <h1>Perfil de Usuario</h1>
+      <p>Nombre: {sesion?.nombre}</p>
+      <button onClick={handleLogout}>Salir</button>
+      <AuthRol superusuario={1}>
         <p>Soy admin!</p>
       </AuthRol>
-      <AuthRol rol="user">
+      <AuthRol superusuario={0} >
         <p>Soy usuario!</p>
       </AuthRol>
+    </div>
     </>
   );
 };
