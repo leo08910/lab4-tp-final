6d9e23c7eacb8873b381f0855443989b0e87f1d6
Alejandro <gmz248alejandro@gmail.com>
2024-11-21 00:43:20 -0300

    Verificación para registrar usuarios agregada

--- front/src/RegistroUsuarios/RegistroUsuarios.jsx
+++ front/src/RegistroUsuarios/RegistroUsuarios.jsx
@@ -1,78 +1,140 @@
 import { useAuth } from "../Auth";
 import { useState } from "react";
+import "./RegistroUsuarios.css";
 
 const RegistroUsuarios = () => {
-    const { sesion } = useAuth();
-    const [registro, setRegistro] = useState({
-        nombre:"",
-        apellido:"",
-        email:"",
-        telefono:"",
-        password:"",
-        superusuario:0
-    });
-    const [mensaje, setMensaje] = useState("")
-    
-     
-    const onSubmit = async (e)=>{
-        e.preventDefault()
-        
-        try {
-            const response = await fetch("http://localhost:3000/usuarios", {
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${sesion.token}`,
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(registro),
-            });
-    
-            if (response.ok) {
-                setMensaje("Usuario registrado"); 
-                setRegistro({ 
-                    nombre: "",
-                    apellido: "",
-                    email: "",
-                    telefono: 0,
-                    password: "",
-                    superusuario: 0,
-                });
-            } else {
-                const errorData = await response.json();
-                setMensaje(`Error: ${errorData.message || "No se pudo registrar el usuario"}`);
-            }
-        } catch (error) {
-            console.error(error);
-            setMensaje("Error de red. Intenta nuevamente.");
-        }
+  const { sesion } = useAuth();
+  const [registro, setRegistro] = useState({
+    nombre: "",
+    apellido: "",
+    email: "",
+    telefono: "",
+    password: "",
+    superusuario: 0,
+  });
+  const [mensaje, setMensaje] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/usuarios", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${sesion.token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(registro),
+      });
+
+      if (response.ok) {
+        setMensaje("Usuario registrado");
+        setRegistro({
+          nombre: "",
+          apellido: "",
+          email: "",
+          telefono: 0,
+          password: "",
+          superusuario: 0,
+        });
+      } else {
+        const errorData = await response.json();
+        setMensaje(
+          `Error: ${errorData.message || "No se pudo registrar el usuario"}`
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      setMensaje("Error de red. Intenta nuevamente.");
     }
+  };
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="nombre">Nombre:</label>
-        <input value={registro.nombre} onChange={(e)=>setRegistro({...registro, nombre: e.target.value})} name="nombre" type="text" required/>
+      {sesion.superusuario === 1 && (
+        <>
+          <form onSubmit={onSubmit} className="registro-usuarios">
+            <div className="inputs-nombre">
+              <input
+                value={registro.nombre}
+                onChange={(e) =>
+                  setRegistro({ ...registro, nombre: e.target.value })
+                }
+                name="nombre"
+                type="text"
+                required
+                placeholder="Nombre"
+              />
 
-        <label htmlFor="apellido">Apellido:</label>
-        <input value={registro.apellido} onChange={(e)=>setRegistro({...registro, apellido: e.target.value})} name="apellido" type="text" required/>
+              <input
+                value={registro.apellido}
+                onChange={(e) =>
+                  setRegistro({ ...registro, apellido: e.target.value })
+                }
+                name="apellido"
+                type="text"
+                required
+                placeholder="Apellido"
+              />
+            </div>
 
-        <label htmlFor="email">Email:</label>
-        <input value={registro.email} onChange={(e)=>setRegistro({...registro, email: e.target.value})} name="email" type="text" required/>
+            <input
+              value={registro.email}
+              onChange={(e) =>
+                setRegistro({ ...registro, email: e.target.value })
+              }
+              name="email"
+              type="email"
+              required
+              placeholder="Correo electrónico"
+            />
 
-        <label htmlFor="telefono">Telefono:</label>
-        <input value={registro.telefono} onChange={(e)=>setRegistro({...registro, telefono: e.target.value})} name="telefono" type="number" min={0} required/>
+            <input
+              value={registro.telefono}
+              onChange={(e) =>
+                setRegistro({ ...registro, telefono: e.target.value })
+              }
+              name="telefono"
+              type="number"
+              min={0}
+              required
+              placeholder="Número de teléfono"
+            />
 
-        <label htmlFor="password">Contraseña:</label>
-        <input value={registro.password} onChange={(e)=>setRegistro({...registro, password: e.target.value})} name="password" type="password" required/>
+            <input
+              value={registro.password}
+              onChange={(e) =>
+                setRegistro({ ...registro, password: e.target.value })
+              }
+              name="password"
+              type="password"
+              required
+              placeholder="Contraseña"
+            />
 
-        <label htmlFor="superusuario">Superusuario:</label>
-        <input value={registro.password} checked={registro.superusuario === 1} onChange={(e)=> setRegistro({...registro, superusuario: e.target.checked ? parseInt(1) : parseInt(0)})} name="superusuario" type="checkbox" />
+            <label htmlFor="superusuario">Superusuario:</label>
+            <input
+              value={registro.password}
+              checked={registro.superusuario === 1}
+              onChange={(e) =>
+                setRegistro({
+                  ...registro,
+                  superusuario: e.target.checked ? parseInt(1) : parseInt(0),
+                })
+              }
+              name="superusuario"
+              type="checkbox"
+            />
 
-        <button type="submit">Registrar usuario</button>
-      </form>
-      <p>{mensaje}</p>
-      
+            <button type="submit">Registrar usuario</button>
+          </form>
+          <p>{mensaje}</p>
+        </>
+      )}
+      {sesion.superusuario != 1 && (
+        <h2>Debe ser superusario para poder registrar otros usuarios</h2>
+      )}
     </>
   );
 };
 
-export default RegistroUsuarios
+export default RegistroUsuarios;
