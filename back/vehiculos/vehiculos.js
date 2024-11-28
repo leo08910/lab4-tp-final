031917e6772a2cb04fc2963113a371c81b43d081
Juan Ignacio Sotomayor <jignaciosotomayor205@gmail.com>
2024-11-28 19:06:24 -0300

    Cambios en la API acorde a los cambios en el front

--- back/vehiculos/vehiculos.js
+++ back/vehiculos/vehiculos.js
@@ -33,28 +33,29 @@ vehiculosRouter.post("/vehiculos", async (req,res)=>{
     .send({vehiculo:{id_vehiculo:sql.insertId,matricula,id_tipo_vehiculo}})
 })
 
-vehiculosRouter.put('/vehiculos/:id_vehiculo/retirar', async (req,res)=>{
-    const {id_vehiculo}=req.params
+vehiculosRouter.put('/vehiculos/retirar', async (req,res)=>{
+    const {matricula}=req.body
 
+    console.log(matricula)
     const [existe]= await db.execute(
-        'select * from vehiculos where id_vehiculo=? and estacionado=1',[id_vehiculo])
+        'select * from vehiculos where matricula=? and estacionado=1',[matricula])
     if (existe.length==0){
         return res.status(404).send('Vehiculo no encontrado')
     }
 
-    await db.execute('update vehiculos set estacionado=0 where id_vehiculo=?',[id_vehiculo])
+    await db.execute('update vehiculos set estacionado=0 where matricula=?',[matricula])
     res.status(200).send('Vehiculo retirado')
 })
 
-/*vehiculosRouter.put('/vehiculos/:id_vehiculo/estacionar', async (req,res)=>{
-    const {id_vehiculo}= req.params
+vehiculosRouter.put('/vehiculos/estacionar', async (req,res)=>{
+    const {matricula}= req.body
 
     const [existe]= await db.execute(
-        'select * from vehiculos where id_vehiculo=? and estacionado=0', [id_vehiculo])
+        'select * from vehiculos where matricula=? and estacionado=0', [matricula])
     if (existe.length==0){
         return res.status(404).send('Este vehiculo no se encuentra estacionado')
     }
 
-    await db.execute('update vehiculos set estacionado=1 where id_vehiculo=?',[id_vehiculo])
+    await db.execute('update vehiculos set estacionado=1 where matricula=?',[matricula])
     res.status(200).send('Vehiculo ingresado')
-})*/
+})
