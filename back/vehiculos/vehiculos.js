b1dad8a0fb3604fb7f61f41bf60609d2538416e8
Juan Ignacio Sotomayor <jignaciosotomayor205@gmail.com>
2024-11-29 10:07:51 -0300

    se movieron las verificaciones al front

--- back/vehiculos/vehiculos.js
+++ back/vehiculos/vehiculos.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import { db } from '../db.js'
-import { ExpressValidator, body, validationResult } from 'express-validator'
 export const vehiculosRouter = express.Router()
 
 
@@ -10,18 +9,7 @@ vehiculosRouter.get('/vehiculos', async (req,res)=>{
 })
 
 vehiculosRouter.post("/vehiculos", async (req,res)=>{
-    const validacion =validationResult(req);
-    if (!validacion.isEmpty()){
-       return res.status(400).send({errores:validacion.array()})
-    }
     const { matricula, id_tipo_vehiculo} = req.body
-
-    const [matriculaRepetida]= await db.execute(
-        'select matricula from vehiculos where matricula=?',[matricula]
-    )
-    if (matriculaRepetida.length>0){
-        return res.status(400).send('Esta matricula ya esta registrada')
-    }
 
     const sql = await db.execute(
         "insert into vehiculos (matricula,id_tipo_vehiculo,estacionado) values(?,?,1)",
@@ -37,11 +25,6 @@ vehiculosRouter.put('/vehiculos/retirar', async (req,res)=>{
     const {matricula}=req.body
 
     console.log(matricula)
-    const [existe]= await db.execute(
-        'select * from vehiculos where matricula=? and estacionado=1',[matricula])
-    if (existe.length==0){
-        return res.status(404).send('Vehiculo no encontrado')
-    }
 
     await db.execute('update vehiculos set estacionado=0 where matricula=?',[matricula])
     res.status(200).send('Vehiculo retirado')
@@ -49,12 +32,6 @@ vehiculosRouter.put('/vehiculos/retirar', async (req,res)=>{
 
 vehiculosRouter.put('/vehiculos/estacionar', async (req,res)=>{
     const {matricula}= req.body
-
-    const [existe]= await db.execute(
-        'select * from vehiculos where matricula=? and estacionado=0', [matricula])
-    if (existe.length==0){
-        return res.status(404).send('Este vehiculo no se encuentra estacionado')
-    }
 
     await db.execute('update vehiculos set estacionado=1 where matricula=?',[matricula])
     res.status(200).send('Vehiculo ingresado')
